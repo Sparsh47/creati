5975c8344161bee3baf90814419b407becfe0c01
sparsh47 <sparshshandilya123@gmail.com>
2025-07-13 22:11:13 +0530

    hanged eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,19 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  {
+    rules: {
+      // no-unused-vars → off
+      "@typescript-eslint/no-unused-vars": "off",
+
+      // no-explicit-any → off
+      "@typescript-eslint/no-explicit-any": "off",
+
+      // hook deps rule → off
+      "react-hooks/exhaustive-deps": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
