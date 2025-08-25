3610415863b394f4e9e7076f1f640f1e663d3651
sparsh47 <sparshshandilya123@gmail.com>
2025-08-25 18:52:43 +0530

    removed features section for now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         <>
             <Navbar />
             <Hero />
-            <Features />
+            {/*<Features />*/}
             <Steps />
             <Testimonials />
             <Pricing />
